20a61698d25b53e794e4a6c0ac5b75374812cf13
puja.exe <puja.exe@gmail.com>
2024-02-08 11:16:21 +0800

    add slick css

--- components/plasmic/penida_tour_host/PlasmicHomepage.tsx
+++ components/plasmic/penida_tour_host/PlasmicHomepage.tsx
@@ -74,6 +74,9 @@ import { useScreenVariants as useScreenVariantst5XXltJfKzLl } from "./PlasmicGlo
 
 import "@plasmicapp/react-web/lib/plasmic.css";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import projectcss from "./plasmic_penida_tour_host.module.css"; // plasmic-import: 8sqboUZ4NdiwTEB5KeNSKm/projectcss
 import sty from "./PlasmicHomepage.module.css"; // plasmic-import: uiobudG9bYmv/css
 
